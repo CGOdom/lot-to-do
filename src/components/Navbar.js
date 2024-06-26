bb1bd2445b3e45988c3e4c14ab63889495b05447
CGOdom <cadengeneodom@gmail.com>
2024-06-26 16:58:59 -0500

    Navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const MyNavbar = () => {
     <Navbar bg="light" expand="lg">
       <Container>
         <LinkContainer to="/">
-          <Navbar.Brand>MyApp</Navbar.Brand>
+          <Navbar.Brand>Lot-To-Do</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
