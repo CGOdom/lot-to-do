1418ac9821a5c58f420c6c9d3a404c8b8d72cf3a
CGOdom <cadengeneodom@gmail.com>
2024-06-26 20:10:35 -0500

    Made items list side-by-side

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import { ListGroup, Button, Form } from 'react-bootstrap';
 
-const TodoList = ({ todos, deleteItem, toggleComplete }) => {
-  return (
-    <ListGroup className="mt-4">
-      {todos.map((todo) => (
-        <ListGroup.Item key={todo.id} className="d-flex justify-content-between align-items-center py-3">
-          <div className="d-flex align-items-center">
-            <Form.Check 
-              type="checkbox"
-              checked={todo.completed}
-              onChange={() => toggleComplete(todo.id)}
-              className="me-3"
-            />
-            <span style={{ textDecoration: todo.completed ? 'line-through' : 'none', flex: 1 }}>
-              {todo.text}
-            </span>
-          </div>
-          <Button
-            variant="danger"
-            onClick={() => deleteItem(todo.id)}
-          >
-            Delete
-          </Button>
-        </ListGroup.Item>
-      ))}
-    </ListGroup>
-  );
-};
+const TodoItem = ({ todo, deleteItem, toggleComplete }) => (
+  <div className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+    <span className="custom-checkbox" onClick={() => toggleComplete(todo.id)} />
+    <span className="todo-text">{todo.text}</span>
+    <Button variant="danger" onClick={() => deleteItem(todo.id)}>Delete</Button>
+  </div>
+);
+
+const TodoList = ({ todos, deleteItem, toggleComplete }) => (
+  <div className="todo-list">
+    {todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} deleteItem={deleteItem} toggleComplete={toggleComplete} />
+    ))}
+  </div>
+);
 
 export default TodoList;
